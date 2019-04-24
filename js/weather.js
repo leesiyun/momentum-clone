fdec37714c0f3b6f0fffe569d18cf247e4b0f3b8
leesiyun <47590181+leesiyun@users.noreply.github.com>
2019-04-24 22:00:41 +0900

    update bg.js

---
+++ js/weather.js
@@ -0,0 +1,35 @@
+const API_KEY = "097c13ae2dfc5420426557ca82d73f41";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  loadedCords === null ? askForCoords() : a;
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
