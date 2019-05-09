e8f43186b16abdb45d5d9d4f1662fd37ad8ff8ea
Siyun Lee <siyun@Siyunui-MacBookPro.local>
2019-05-09 16:26:44 +0900

    random bg

--- js/bg.js
+++ js/bg.js
@@ -2,25 +2,29 @@ $(document).ready(function() {
   "use strict";
 
   const body = document.querySelector("body");
+  const image = new Image();
+  image.src = `https://source.unsplash.com/1600x900/?nature,water`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
 
-  const IMG_NUMBER = 4;
+  // const IMG_NUMBER = 4;
 
-  function paintImage(imgNumber) {
-    const image = new Image();
-    image.src = `images/${imgNumber + 1}.jpg`;
-    image.classList.add("bgImage");
-    body.appendChild(image);
-  }
+  // function paintImage(imgNumber) {
+  //   const image = new Image();
+  //   image.src = `images/${imgNumber + 1}.jpg`;
+  //   image.classList.add("bgImage");
+  //   body.appendChild(image);
+  // }
 
-  function genRandom() {
-    const number = Math.floor(Math.random() * IMG_NUMBER);
-    return number;
-  }
+  // function genRandom() {
+  //   const number = Math.floor(Math.random() * IMG_NUMBER);
+  //   return number;
+  // }
 
-  function init() {
-    const randomNumber = genRandom();
-    paintImage(randomNumber);
-  }
+  // function init() {
+  //   const randomNumber = genRandom();
+  //   paintImage(randomNumber);
+  // }
 
-  init();
+  // init();
 });
