6789c0a780324834560f3d42304b70a5ec8665a6
Siyun Lee <siyun@Siyunui-MacBookPro.local>
2019-05-09 11:45:22 +0900

    Update bg.js

--- js/bg.js
+++ js/bg.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
   function paintImage(imgNumber) {
     const image = new Image();
-    image.src = `/images/${imgNumber + 1}.jpg`;
+    image.src = `images/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.appendChild(image);
   }
